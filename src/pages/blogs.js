57ec11bfdf419aefac5b4afe18fec7bdc4662555
Anksus <sankitpramod.min18@itbhu.ac.in>
2020-11-10 00:10:24 +0530

    navbar

---
+++ src/pages/blogs.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Layout from "../components/layout";
+
+const Blog = () => {
+  return (
+    <Layout>
+      <div>
+        <h1>This is blogs page</h1>
+      </div>
+    </Layout>
+  );
+};
+export default Blog;
---
+++ src/pages/events.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../components/layout";
+
+const Event = () => {
+  return (
+    <Layout>
+      <h1>Event page</h1>
+    </Layout>
+  );
+};
+
+export default Event;
