23c8fa7134a5bd5b48f56f1d9b9276c59739b132
Shashank Pathak <pathakshashank32414@gmail.com>
2020-11-12 10:57:24 +0530

    add: Social media card component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,36 @@
 import React from "react";
 import Layout from "../components/layout";
+import Social from "../components/social";
+
+const platforms = [
+  {
+    name: "Instagram",
+    imgLink: "https://www.flaticon.com/svg/static/icons/svg/2111/2111463.svg",
+    desc: "Galore of the astronomical lore",
+    link: "https://www.instagram.com/astro.iitbhu/?hl=en",
+    linkText: "Follow us"
+  },
+  {
+    name: "Facebook",
+    imgLink: "https://www.flaticon.com/svg/static/icons/svg/145/145802.svg",
+    desc: "We post on FB too!",
+    link: "https://www.facebook.com/astro.iitbhu/",
+    linkText: "Visit the page"
+  },
+  {
+    name: "Twitter",
+    imgLink: "https://www.flaticon.com/svg/static/icons/svg/733/733579.svg",
+    desc: "Listen to our tweet",
+    link: "https://twitter.com/",
+    linkText: "Take me there"
+  }
+]
 
 export default () => (
   <Layout>
     <h1>Home</h1>
-    <h1>Home</h1>
-    <h1>Home</h1>
+    <Social
+      platform={platforms}
+      />
   </Layout>
 );
