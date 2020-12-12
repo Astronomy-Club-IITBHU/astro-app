c4ed05290ad7f76293a95e1e1aa81b1560e16cf6
Anksus <sankitpramod.min18@itbhu.ac.in>
2020-12-12 20:14:33 +0530

    refactor

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Layout from "../components/layout";
-import RecentPost from "../hooks/use-recent-post";
+import RecentPost from "../hooks/all-blog";
 import PostPreview from "../components/post-preview";
 
 // const platforms = [
@@ -23,37 +23,37 @@ import PostPreview from "../components/post-preview";
 const Front = () => {
   return (
     <>
-    <section className="text-gray-300 body-font">
-      <div className="container mx-auto px-5 py-15 flex flex-col  md:items-start md:text-left items-center">
-        <h1 className="title-font sm:text-4xl text-3xl mb-1 font-medium text-gray-200 ">
-          Since its foundation
-          <br className="hidden lg:inline-block" /> in{" "}
-          <span className="font-mono font-semibold ">2012</span>
-        </h1>
-        <p className="mb-8 leading-relaxed">
-          The Astronomy club aims to build on this fascination and brings
-          innovation and passion to its vision of spreading the awareness and
-          knowledge of astronomy and space sciences to the students. This forum
-          is built to spread the facts and news about all the events taking part
-          in the realm of Astronomy and the events being conducted within the
-          club in and out the campus.
-          <br className="hidden lg:inline-block" />
-          <br className="hidden lg:inline-block" />
-          If you have ever been curious to know the secrets preserved in this
-          universe's lap, if watching the night sky provides you the indigenous
-          curiosity, then you're at the right place. The Astronomy club IIT
-          BHU, an ambitious team driven by amateur astronomers, strives to quench
-          a student’s thirst for astronomy by organizing various exciting events
-          including{" "}
-          <span className="font-semibold">
-            Observational Sessions, Corona magazine, Astro-quiz, Telescon
-          </span>
-          , and many more. We do conduct online and offline workshops as well for
-          students to introduce them to basics of Astronomy.
-        </p>
-      </div>
+      <section className="text-gray-300 body-font">
+        <div className="container mx-auto px-5 py-15 flex flex-col  md:items-start md:text-left items-center">
+          <h1 className="title-font sm:text-4xl text-3xl mb-1 font-medium text-gray-200 ">
+            Since its foundation
+            <br className="hidden lg:inline-block" /> in{" "}
+            <span className="font-mono font-semibold ">2012</span>
+          </h1>
+          <p className="mb-8 leading-relaxed">
+            The Astronomy club aims to build on this fascination and brings
+            innovation and passion to its vision of spreading the awareness and
+            knowledge of astronomy and space sciences to the students. This
+            forum is built to spread the facts and news about all the events
+            taking part in the realm of Astronomy and the events being conducted
+            within the club in and out the campus.
+            <br className="hidden lg:inline-block" />
+            <br className="hidden lg:inline-block" />
+            If you have ever been curious to know the secrets preserved in this
+            universe's lap, if watching the night sky provides you the
+            indigenous curiosity, then you're at the right place. The Astronomy
+            club IIT BHU, an ambitious team driven by amateur astronomers,
+            strives to quench a student’s thirst for astronomy by organizing
+            various exciting events including{" "}
+            <span className="font-semibold">
+              Observational Sessions, Corona magazine, Astro-quiz, Telescon
+            </span>
+            , and many more. We do conduct online and offline workshops as well
+            for students to introduce them to basics of Astronomy.
+          </p>
+        </div>
       </section>
-      </>
+    </>
   );
 };
 
@@ -64,8 +64,10 @@ export default () => {
     <Layout>
       <Front />
       <div>
-        <h1 className="text-gray-300 max-w-2xl text-4xl mx-auto px-8 py-4 ">Recent Posts</h1>
-        {recentPost.map(post => (
+        <h1 className="text-gray-300 max-w-2xl text-4xl mx-auto px-8 py-4 ">
+          Recent Posts
+        </h1>
+        {recentPost.slice(0, 3).map(post => (
           <PostPreview key={post.slug} post={post} />
         ))}
       </div>
