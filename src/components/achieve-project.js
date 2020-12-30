07003b653582cd63d28500d3b542d0299697f4bd
Shashank Pathak <64695775+pathakshashank17@users.noreply.github.com>
2020-12-31 00:31:24 +0530

    Projects & Achievements (#18)
    * Changes to be committed:
    	achieve-project.js: Built template
    	misc.js: Added more data on secys
    
    * Changes to be committed:
    	achieve-project.js: Added achievements & projects
    	secys.js: Created component for secretaries
    	misc.js: Added the secretaries component in accordian
    
    * del: package-lock.json

--- src/components/achieve-project.js
+++ src/components/achieve-project.js
@@ -1,70 +1,98 @@
 import React from "react";
 
 const AchieveProject = () => {
+  const achievements = [
+    {
+      name: "NSSC 2019, IIT KGP",
+      events: [
+        {
+          eventName: "Case Study",
+          result: ["1st (Yash Vinod & Vaibhav Singh)", "3rd (Vamsi Bodaballa & Aditya Keshari)"]
+        },
+        {
+          eventName: "The Science of Paper Folding",
+          result: ["1st (Arjeet Dewanagan & Anmol Saluja)", "3rd (Yash Vinod & Vaibhav Singh)"]
+        }
+      ]
+    },
+    {
+      name: "NSSC 2018, IIT KGP",
+      events: [
+        {
+          eventName: "Case Study",
+          result: ["1st (Ayush Kumar Singh & Shristi Singh)"]
+        },
+        {
+          eventName: "Designeer",
+          result: ["3rd (Rahul Viswakarma, Pranjal & Yash Agrawal)"]
+        },
+        {
+          eventName: "The Science of Paper Folding",
+          result: ["2nd (Yash Vinod & Vaibhav Krishna)"]
+        }
+      ]
+    },
+    {
+      name: "Inter IIT Tech Meet",
+      events: [
+        {
+          eventName: "Star Cluster Identifying",
+          result: ["3rd (Sambhav Jain, Yash Agrawal, Abhishek & Govindswaroop Rahangdale)"]
+        }
+      ]
+    }
+  ]
+
+  const projects = [
+    {
+      name: "Planetarium",
+      desc: "With the support of SERAI, the Astronomy Club has undertaken the project to build a planetarium to promote astronomy culture within the college. Being one of the first steps to enter into astronomy, the planetarium helps to attract people into amateur astronomy. The planetarium serves the purpose of observing the night sky of Varanasi as well as other regions of the world regardless of weather conditions and time of the day. The planetarium will have sufficient space for 15-20 people to sit inside comfortably. The planetarium will be used primarily to conduct observational sessions. It will also be used to conduct various competitions and events throughout the year."
+    },
+    {
+      name: "Horn Antenna",
+      desc: "Radio astronomy is a major field in observational and computational astronomy. There are many types of antennae to observe radio waves, the most basic which is a Horn Antenna. To encourage advanced astronomy in the institute, the Astronomy Club is building its own Pyramidal Horn Antenna. Upon its completion, the club will be able to conduct its own radio observations and analyze and study the data obtained."
+    }
+  ]
   return (
-    <div className="lg:w-2/3 rounded-lg bg-gray-900 md:w-1/2 px-5 py-5 mx-10 mb-5 bg-white ">
-      <h1 className="text-2xl	text-center pb-5">Projects</h1>
-      <p>
-        {" "}
-        The Astronomy club tries to bring out the inner astronomer that has been
-        hiding inside for so long. We have three major visions for the club:
-        Through our workshops and night sessions we wish to build up curiosity
-        and knowledge about the universe around us. Getting the astronomy
-        fanatics of the college educated and more comfortable all aspects of
-        astronomy from observational astronomy and telescope handling to Radio
-        Astronomy and other computational aspects. Propagating the ideas of last
-        year and bringing forth new ideas. Asteroid mining was a revolutionary
-        idea presented in a competition last year and we wish to propagate it
-        further as well as get new ideas. In the following academic year, the
-        club will be trying to get its own planetarium for various training and
-        research purposes. We are also looking forward to collaborate with
-        various other clubs and colleges to bring new and exquisite knowledge in
-        the club. The Astronomy club tries to bring out the inner astronomer
-        that has been hiding inside for so long. We have three major visions for
-        the club: Through our workshops and night sessions we wish to build up
-        curiosity and knowledge about the universe around us. Getting the
-        astronomy fanatics of the college educated and more comfortable all
-        aspects of astronomy from observational astronomy and telescope handling
-        to Radio Astronomy and other computational aspects. Propagating the
-        ideas of last year and bringing forth new ideas. Asteroid mining was a
-        revolutionary idea presented in a competition last year and we wish to
-        propagate it further as well as get new ideas. In the following academic
-        year, the club will be trying to get its own planetarium for various
-        training and research purposes. We are also looking forward to
-        collaborate with various other clubs and colleges to bring new and
-        exquisite knowledge in the club. knowledge about the universe around us.
-        Getting the astronomy fanatics of the college educated and more
-        comfortable all aspects of astronomy from observational astronomy and
-        telescope handling to Radio Astronomy and other computational aspects.
-        Propagating the ideas of last year and bringing forth new ideas.
-        Asteroid mining was a revolutionary idea presented in a competition last
-        year and we wish to propagate it further as well as get new ideas. In
-        the following academic year, the club will be trying to get its own
-        planetarium for various training and research purposes. We are also
-        looking forward to collaborate with various other clubs and colleges to
-        bring new and exquisite knowledge in the club. the club. knowledge about
-        the universe around us. Getting the astronomy fanatics of the college
-        educated and more comfortable all aspects of astronomy from
-        observational astronomy and telescope handling to Radio Astronomy and
-        other computational aspects. Propagating the ideas of last year and
-        bringing forth new ideas. Asteroid mining was a revolutionary idea
-        presented in a competition last year and we wish to propagate it further
-        as well as get new ideas. In the following academic year, the club will
-        be trying to get its own planetarium for various training and research
-        purposes. We are also looking forward to collaborate with various other
-        clubs and colleges to bring new and exquisite knowledge in the club.the
-        club. knowledge about the universe around us. Getting the astronomy
-        fanatics of the college educated and more comfortable all aspects of
-        astronomy from observational astronomy and telescope handling to Radio
-        Astronomy and other computational aspects. Propagating the ideas of last
-        year and bringing forth new ideas. Asteroid mining was a revolutionary
-        idea presented in a competition last year and we wish to propagate it
-        further as well as get new ideas. In the following academic year, the
-        club will be trying to get its own planetarium for various training and
-        research purposes. We are also looking forward to collaborate with
-        various other clubs and colleges to bring new and exquisite knowledge in
-        the club.
-      </p>
+    <div className="lg:w-2/3 rounded-lg bg-gray-900 md:w-1/2 px-5 py-5 mx-10 mb-5 bg-white px-10">
+
+      <div className="text-2xl text-center pb-2 mt-5">Projects</div>
+      <hr style={{ borderTop: "1px dotted lightgray" }}></hr>
+      {
+        projects.map(data => {
+          return (
+            <div class="my-3">
+              <div class="inline text-xl">{data.name}</div>
+              <p class="my-1 text-gray-200">{data.desc}</p>
+            </div>
+          )
+        })
+      }
+
+      <div className="text-2xl	text-center pb-2">Recent Achievements</div>
+      <hr style={{ borderTop: "1px dotted lightgray" }}></hr>
+      {
+        achievements.map(data => {
+          return (
+            <div class="my-3">
+              <div class="inline my-2 text-xl">{data.name}</div>
+              {
+                data.events.map(event => {
+                  return (
+                    <ul class="px-5 list-disc list-inside pb-3">
+                      <li class="text-lg text-gray-200">{event.eventName}</li>
+                      <div>
+                        {event.result.map(val => { return (<div class="pl-7 text-gray-300">{val}</div>) })}
+                      </div>
+                    </ul>
+                  )
+                })
+              }
+            </div>
+          )
+        })
+      }
+
     </div>
   );
 };
