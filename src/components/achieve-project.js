9f4ca6b28040c37ea4a26e3f37b362d3143a76a2
Shashank Pathak <pathakshashank32414@gmail.com>
2020-12-31 11:23:32 +0530

    added planetarium image

--- src/components/achieve-project.js
+++ src/components/achieve-project.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { graphql, useStaticQuery } from "gatsby";
+import Img from "gatsby-image";
 
 const AchieveProject = () => {
   const achievements = [
@@ -53,17 +55,45 @@ const AchieveProject = () => {
       desc: "Radio astronomy is a major field in observational and computational astronomy. There are many types of antennae to observe radio waves, the most basic which is a Horn Antenna. To encourage advanced astronomy in the institute, the Astronomy Club is building its own Pyramidal Horn Antenna. Upon its completion, the club will be able to conduct its own radio observations and analyze and study the data obtained."
     }
   ]
+
+  const projectImgs = useStaticQuery(graphql`
+    query {
+      allFile(filter: { relativeDirectory: { eq: "projects" } }, sort: { fields: [name], order: ASC }) {
+        edges {
+          node {
+            childImageSharp {
+              fluid {
+                ...GatsbyImageSharpFluid_withWebp
+              }
+            }
+          }
+        }
+      }
+    }`
+  );
+
   return (
     <div className="lg:w-2/3 rounded-lg bg-gray-900 md:w-1/2 px-5 py-5 mx-10 mb-5 bg-white px-10">
 
       <div className="text-2xl text-center pb-2 mt-5">Projects</div>
       <hr style={{ borderTop: "1px dotted lightgray" }}></hr>
       {
-        projects.map(data => {
+        projects.map((data, index) => {
           return (
             <div class="my-3">
               <div class="inline text-xl">{data.name}</div>
               <p class="my-1 text-gray-200">{data.desc}</p>
+              {
+                index === 0 &&
+                <div className="mx-auto mt-2 w-full w-1">
+                  <Img
+                    alt={data.name}
+                    class="absolute inset-0 w-full h-full object-cover object-center"
+                    fluid={projectImgs.allFile.edges[0].node.childImageSharp.fluid}
+                    style={{ height: "100%", width: "100%" }}
+                  />
+                </div>
+              }
             </div>
           )
         })
