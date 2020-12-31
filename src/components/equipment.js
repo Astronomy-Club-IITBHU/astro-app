cbf85a1ba6aee8f0928e22e925ee6c39360d153c
Shashank Pathak <64695775+pathakshashank17@users.noreply.github.com>
2020-12-31 19:35:14 +0530

    Changed Equipments table (#22)
    * Changes to be committed:
    	achieve-project.js: Built template
    	misc.js: Added more data on secys
    
    * Changes to be committed:
    	achieve-project.js: Added achievements & projects
    	secys.js: Created component for secretaries
    	misc.js: Added the secretaries component in accordian
    
    * del: package-lock.json
    
    * added planetarium image
    
    * added planetarium image in projects & box shadow in misc
    
    * merge upstream/main
    
    * changed table design of Equipments
    
    * added padding

--- src/components/equipment.js
+++ src/components/equipment.js
@@ -1,65 +1,61 @@
 import React from "react";
 
 const Table = () => {
-  const data = [
-    [
-      "Meade LX200 (blue)",
-      "Telescope",
-      "10 aperture, 6'' dia schmidt-cassegrain design. In its case on TKO platform.",
-    ],
-    [
-      "Orion SkyQuest (dark gray)",
-      "Telescope",
-      "10 aperture, 12'' dia, Dobsonian design. Currently mounted in TKO, replacing the Bobich scope.	",
-    ],
-    [
-      "Meade LXD55 (white)",
-      "Telescope",
-      "One 10 aperture, One 8 aperture, both 90mm dia, Schmidt-Newtonian design.",
-    ],
-    [
-      "Astromaster 102AZ (blue)",
-      "Telescope",
-      "10 aperture, 5'' dia, Schmidt-cassegrain design, Refractor type",
-    ],
-    [
-      "Orion 190mm (white)",
-      "Telescope",
-      "10 aperture, 6'' dia, Maksutov Newtonian design, ",
-    ],
+  const telescopes = [
+    {
+      name: "Meade LX200",
+      desc: "10 aperture, 6'' dia Schmidt-Cassegrain design, in its case on TKO platform."
+    },
+    {
+      name: "Orion SkyQuest",
+      desc: "10 aperture, 12'' dia, Dobsonian design. Currently mounted in TKO, replacing the Bobich scope."
+    },
+    {
+      name: "Meade LXD55",
+      desc: "One 10 aperture, One 8 aperture, both 90mm dia, Schmidt-Newtonian design."
+    },
+    {
+      name: "Astromaster 102AZ",
+      desc: "10 aperture, 5'' dia, Schmidt-Cassegrain design, Refractor type"
+    },
+    {
+      name: "Orion 190mm",
+      desc: "10 aperture, 6'' dia, Maksutov-Newtonian design"
+    }
   ];
 
-  const heading = ["Name", "Category", "Description"];
-
   return (
-    <div className="text-white w-full sm:place-content-evenly">
-      <table className="bg-gray-900 rounded-lg	border-separate	p-2	shadow-md w-full text-md md:text-lg lg:text-lg">
-        <thead>
-          <tr className="border-t-1">
-            {heading.map(ele => (
-              <th className="border-b-2 text-center">{ele}</th>
-            ))}
-          </tr>
-        </thead>
-        <tbody className="">
-          {data.map(ele => (
-            <React.Fragment>
-              <tr className="transition duration-500 ease-in-out hover:bg-white hover:text-black hover:shadow-2xl">
-                <td className="text-center border-b-2 border-gray-500 rounded-lg md:border-b-0 lg:border-b-0">
-                  <div className="my-5 lg:m-2 md:m-2 sm:m-3">{ele[0]}</div>
-                </td>
-                <td className="text-center border-b-2 border-gray-500 rounded-lg md:border-b-0 lg:border-b-0">
-                  <div className="my-5 lg:m-2 md:m-2 sm:m-3">{ele[1]}</div>
-                </td>
-                <td className="text-center border-b-2 border-gray-500 rounded-lg md:border-b-0 lg:border-b-0">
-                  <div className="my-5 lg:m-2 md:m-2 sm:m-3">{ele[2]}</div>
-                </td>
-              </tr>
-            </React.Fragment>
-          ))}
-        </tbody>
-      </table>
-    </div>
+    <div class="mx-auto px-4 sm:px-8">
+      <div class="py-8 w-5/6 mx-auto">
+        <div class="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
+          <div class="inline-block min-w-full overflow-hidden">
+            <div class="flex justify-center my-5">
+              <div class="text-gray-200 text-4xl">Telescopes</div>
+            </div>
+            <table class="min-w-full leading-normal mb-10">
+              <tbody>
+                {
+                  telescopes.map((data, index) => {
+                    return (
+                      <tr>
+                        <td class={`flex items-center justify-around py-5 border-b border-gray-200 bg-gray-300 text-md ${(index === 0) ? `rounded-t-lg` : (index === telescopes.length - 1) ? `rounded-b-lg` : ``}`}>
+                          <div class="w-1/2 text-center pr-2">
+                            <p class="text-gray-900 font-semibold">{data.name}</p>
+                          </div>
+                          <div class="w-1/2 text-center">
+                            <p class="relative pr-2 text-gray-800 text-sm lg:pr-5">{data.desc}</p>
+                          </div>
+                        </td>
+                      </tr>
+                    )
+                  })
+                }
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div >
   );
 };
 
