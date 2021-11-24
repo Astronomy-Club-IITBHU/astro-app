8af677ed487953b42a062942e73745780620c0c5
pathakshashank17 <pathakshashank32414@gmail.com>
2021-11-24 18:11:42 +0530

    add: Astropedia Schedule

--- src/components/recent-events.js
+++ src/components/recent-events.js
@@ -5,7 +5,7 @@ import Img from "gatsby-image";
 const UpcomingEvents = () => {
   const data = useStaticQuery(graphql`
     query {
-      file(relativePath: { eq: "astromeme.jpg" }) {
+      file(relativePath: { eq: "astropedia.jpg" }) {
         childImageSharp {
           fluid {
             ...GatsbyImageSharpFluid_withWebp
@@ -30,31 +30,19 @@ const UpcomingEvents = () => {
     >
       <h1 className="text-2xl	text-center pb-5">Recent Events</h1>
       <hr style={{ borderTop: "1px dotted lightgray" }}></hr>
-      <p className="my-3">
-        <p className="my-2 text-center">ASTROMEME '21</p>
+      <p className="my-3 flex flex-col">
+        <p className="mb-3 text-center text-xl">ASTROPEDIA</p>
         <Img
-          alt={"Astromeme '21"}
+          alt={"Astropedia Schedule"}
           class="absolute inset-0 w-full h-full object-cover object-center"
           fluid={
             image
           }
           style={{ height: "100%", width: "100%" }}
         />
-        <p className="my-1">Winning entries:</p>
-        <ol>
-          <li>1. {' '}
-            <a class="transform duration-200 text-gray-300 hover:text-gray-100" href="https://www.instagram.com/adityasurana7/">Aditya Surana</a> {' '}
-            (Entry <a class="transform duration-200 p-1 text-sm text-gray-300 bg-gray-800 hover:text-gray-100"href="https://www.instagram.com/p/CVVhw9XIO1w/">#5</a>)
-          </li>
-          <li>2. {' '}
-            <a class="transform duration-200 text-gray-300 hover:text-gray-100" href="https://www.instagram.com/kumar._.vikrant/">Vikrant</a> {' '}
-            (Entry <a class="transform duration-200 p-1 text-sm text-gray-300 bg-gray-800 hover:text-gray-100" href="https://www.instagram.com/p/CVVQb2HIDNm/">#4</a>)
-          </li>
-          <li>3. {' '}
-            <a class="transform duration-200 text-gray-300 hover:text-gray-100" href="https://www.instagram.com/yoshita.vallecha17/">Yoshita Vallecha</a> {' '}
-            (Entry <a class="transform duration-200 p-1 text-sm text-gray-300 bg-gray-800 hover:text-gray-100" href="https://www.instagram.com/p/CVdP8VLBg0H/">#21</a>)
-          </li>
-        </ol>
+        <button class="mt-5 bg-indigo-600 bg-opacity-50 transition duration-300 hover:bg-blue-500 text-white-700 font-semibold hover:text-white py-2 px-4 hover:border-transparent rounded">
+          <a href="http://bit.ly/Astropedia">Register Now!</a>
+        </button>
       </p>
     </div>
   );
