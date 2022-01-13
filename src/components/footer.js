5aa6a37365d10347aa4b7d87160c0b1d5371f3fb
pathakshashank17 <pathakshashank32414@gmail.com>
2022-01-13 22:16:08 +0530

    upd: Recent Events - Merch drop, fix warns

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,11 +5,11 @@ const Footer = () => {
   return (
     <footer className="bg-gray-900 ">
       <div className="container px-5 py-8 mx-auto max-w-3xl flex items-center sm:flex-row flex-col">
-        <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-500">
+        <span className="flex title-font font-medium items-center md:justify-start justify-center text-gray-500">
           <AstroSVG color="white" />
-        </a>
+        </span>
         <p className="text-sm text-gray-400 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
-          © 2021 IITBHU
+          © 2022 IITBHU
         </p>
         <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
           {/* ----------------------   For Facebook  -------------------- */}
--- src/components/recent-events.js
+++ src/components/recent-events.js
@@ -1,29 +1,27 @@
 import React from "react";
-import { graphql, useStaticQuery, Link } from "gatsby";
+import { graphql, useStaticQuery } from "gatsby";
 import Img from "gatsby-image";
-
 const UpcomingEvents = () => {
   const data = useStaticQuery(graphql`
-    query {
-      file(relativePath: { eq: "Orientation.jpeg" }) {
-        childImageSharp {
-          fluid {
-            ...GatsbyImageSharpFluid_withWebp
+    {
+      images: allFile(filter: {relativeDirectory: {eq: "merch"}}) {
+        nodes {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
           }
+          name
         }
       }
     }
   `);
-  const image = data.file.childImageSharp.fluid;
-
-  console.log(image);
 
   return (
     <div
       className="flex-grow lg:w-1/4 rounded-lg bg-gray-900 md:w-1/2 px-5 py-5 bg-white overflow-hidden mb-5 font-mono"
       style={{
         height: "1%",
-        position: "-webkit-sticky",
         position: "sticky",
         top: "0",
       }}
@@ -31,17 +29,17 @@ const UpcomingEvents = () => {
       <h1 className="text-2xl	text-center pb-5">Recent Events</h1>
       <hr style={{ borderTop: "1px dotted lightgray" }}></hr>
       <p className="my-3 flex flex-col">
-        <p className="mb-3 text-center text-xl">Freshers' Orientation</p>
-        <Img
-          alt={"Orientation"}
-          class="absolute inset-0 w-full h-full object-cover object-center"
-          fluid={
-            image
-          }
-          style={{ height: "100%", width: "100%" }}
-        />
+        <p className="mb-3 text-center text-xl">Merch Launch</p>
+        {data.images.nodes.map(image => (
+          <Img
+            alt={image.name}
+            class="absolute inset-0 w-full h-full object-cover object-center"
+            fluid={image.childImageSharp.fluid}
+            style={{ height: "100%", width: "100%" }}
+          />
+        ))}
         <button class="mt-5 bg-indigo-600 bg-opacity-50 transition duration-300 hover:bg-blue-500 text-white-700 font-semibold hover:text-white py-2 px-4 hover:border-transparent rounded">
-          <a href="https://bit.ly/Astro-Orientation">Join Us!</a>
+          <a href="https://forms.gle/37aAR4bW34BxLYTF9">Order Now</a>
         </button>
       </p>
     </div>
